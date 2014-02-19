1b150b3fd7eb361330f3dacabc4c0a743754d12c
Vasya Pupkin <plumqqz@yahoo.com>
2014-02-19 23:45:35 +0400

    Add schema qualification to helper migration script

--- trunk/prepare_for_dump.sql
+++ trunk/prepare_for_dump.sql
@@ -6,8 +6,8 @@ update pg_catalog.pg_extension set extconfig=t.config, extcondition=t.condition
     ) as t
 where extname='mbus';
 
-alter extension mbus drop sequence seq;
-alter extension mbus drop sequence qt_model_id_seq;
-alter extension mbus drop sequence consumer_id_seq;
-alter extension mbus drop sequence queue_id_seq;
+alter extension mbus drop sequence mbus.seq;
+alter extension mbus drop sequence mbus.qt_model_id_seq;
+alter extension mbus drop sequence mbus.consumer_id_seq;
+alter extension mbus drop sequence mbus.queue_id_seq;
 grant usage on schema mbus to public;
